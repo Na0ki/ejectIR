b2569aa2b225cc53b17788b013e34facdda7abdd
ahiru <nmaeda-nit24@yb4.so-net.ne.jp>
2014-08-01 12:30:02 +0900

    Update ejectIR.py

--- ejectIR.py
+++ ejectIR.py
@@ -11,8 +11,8 @@ GPIO.setup(4, GPIO.IN)
 while 1:
 	if GPIO.input(4):
 		os.system("eject")
-		print "Welcome to my room!!"
-		time.sleep(1)
+#		print "Welcome to my room!!"
+#		time.sleep(1)
 		os.system("eject -t")
 		continue
 	else:
